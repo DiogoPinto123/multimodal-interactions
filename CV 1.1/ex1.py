a38cd7aff0f064e92de4a772c2a28741160f4e17
Diogo Pinto <m54062@alunos.uevora.pt>
2023-06-11 12:02:05 +0100

    Simple image reading and display with OpenCV

---
+++ CV 1.1/ex1.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+img = cv.imread("messi.jpg")
+cv.imshow('windows', img)
+
+cv.imwrite('messi.png', img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imwrite('messi_gray.png', gray)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
