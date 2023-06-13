2bdbd625a38a87558466cfe632da6d738fe04378
Diogo Pinto <diogopintotablet@gmail.com>
2023-06-13 01:25:24 +0100

    images weighted sum and bitwise operations

---
+++ CV 2.2/images_weighted_sum.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+img1 = cv.imread('img1.png')
+img2 = cv.imread('img2.jpg')
+assert img1 is not None, "file could not be read, check with os.path.exists()"
+assert img2 is not None, "file could not be read, check with os.path.exists()"
+
+# Weighted sum of 2 images
+dst = cv.addWeighted(img1,0.7,img2,0.3,0)
+cv.imshow('dst',dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
